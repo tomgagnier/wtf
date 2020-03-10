aa17444e80c20ed63f5f1896469cb28a17cd167b
Tom Gagnier (tgagnier) <tgagnier@cisco.com>
2020-03-10 16:54:14 -0600

    Test Timer using injected timer

--- unit-testing-with-lambdas/src/test/java/org/wtf/TimerTest.java
+++ unit-testing-with-lambdas/src/test/java/org/wtf/TimerTest.java
@@ -1,5 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.wtf;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TimerTest {
 
+    @Test void lifecycle() {
+        List<Long> times = new ArrayList<Long>(asList(100L, 200L));
+
+        Timer timer = new Timer(() -> {return times.remove(0);});
+
+        timer.start();
+
+        assertEquals(100, timer.elapsed());
+    }
 }
